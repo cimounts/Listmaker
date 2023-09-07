85bcb9361820efc621eaa2eebe897387ff8a8123
Christian Mounts <christian.mounts@gmail.com>
2023-09-06 21:01:10 -0400

    Chapter 8 End

--- app/src/main/java/com/raywenderlich/listmaker/ui/main/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ui/main/ListSelectionRecyclerViewAdapter.kt
@@ -3,9 +3,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.listmaker.databinding.ListSelectionViewHolderBinding
+import com.raywenderlich.listmaker.models.TaskList
 
-class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter <ListSelectionViewHolder>() {
-    val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
+class ListSelectionRecyclerViewAdapter(private val lists : MutableList<TaskList>) : RecyclerView.Adapter<ListSelectionViewHolder>(){
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         val binding =
@@ -20,10 +21,13 @@ class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter <ListSelectionView
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
         holder.binding.itemNumber.text = (position + 1).toString()
-        holder.binding.itemString.text = listTitles[position]
+        holder.binding.itemString.text = lists[position].name
     }
 
     override fun getItemCount(): Int {
-        return listTitles.size
+        return lists.size
+    }
+    fun listUpdated(){
+        notifyItemInserted(lists.size-1)
     }
 }
--- app/src/main/java/com/raywenderlich/listmaker/ui/main/MainFragment.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ui/main/MainFragment.kt
@@ -2,39 +2,48 @@ package com.raywenderlich.listmaker.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.listmaker.R
 import com.raywenderlich.listmaker.databinding.MainFragmentBinding
-
+import com.raywenderlich.listmaker.models.TaskList
+import kotlin.properties.Delegates
 
 class MainFragment : Fragment() {
+
     private lateinit var binding: MainFragmentBinding
+
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = MainFragmentBinding.inflate(inflater, container, false)
-        binding.listsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
 
-        binding.listsRecyclerview.adapter = ListSelectionRecyclerViewAdapter()
+        binding.listsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
 
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        viewModel = ViewModelProvider(requireActivity(),
+            MainViewModelFactory(PreferenceManager.getDefaultSharedPreferences(requireActivity())))
+            .get(MainViewModel::class.java)
 
+        val recyclerViewAdapter = ListSelectionRecyclerViewAdapter(viewModel.lists)
+
+        binding.listsRecyclerview.adapter = recyclerViewAdapter
+
+        viewModel.onListAdded = {
+            recyclerViewAdapter.listUpdated()
+        }
+    }
 }
